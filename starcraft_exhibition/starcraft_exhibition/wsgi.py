c8477f92011688c29e62f2ea814e482d45f39633
QiyueSheng <dereksheng1226@gmail.com>
2017-05-09 23:25:24 -0400

    initial project with hello world page

---
+++ starcraft_exhibition/starcraft_exhibition/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for starcraft_exhibition project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "starcraft_exhibition.settings")
+
+application = get_wsgi_application()
